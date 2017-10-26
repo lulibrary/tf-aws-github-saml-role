1b39c09f75f987b31a28561e465208830e573faa
Stephen Robinson <stephen@stephen-robinson.co.uk>
2017-10-26 12:54:07 +0100

    Update input variables and output descriptions

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "role_arn" {
     value = "${aws_iam_role.saml_role.arn}"
+    description  = "ARN of the AWS role created for the SAML provider"
 }
 
 output "github_team_id" {
     value = "${github_team.saml_role_github_team.id}"
+    description = "ID of the GitHub team created to control access to this AWS role"
 }
